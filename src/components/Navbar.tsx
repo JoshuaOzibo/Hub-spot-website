8eb8b6f521b4abd112ebba250243540b3e235d06
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-01-16 05:18:13 +0100

    completed navigation bar section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,45 @@
 import Button from "./Button";
+import { IoIosArrowDown } from "react-icons/io";
+import HubspotFullLogo from "../assets/logo/Hubspot_Full_Logo.svg";
+import HubspotLogo from "../assets/logo/Hubspot_Logo.svg";
 
 const Navbar = () => {
   return (
-    <nav>
-      <a href="" aria-label="HubSpot">
-        <header>HubSpot</header>
-      </a>
+    <nav className="flex lg:px-40 sm:px-4 px-2 items-center justify-between">
+      <div className="flex items-center gap-8">
+        <a href="" aria-label="HubSpot">
+          <img src={HubspotFullLogo} alt="HubspotLogo" className="w-20 h-10" />
+        </a>
 
-      <ul>
-        <li>Products </li>
-        <li>Solutions</li>
-        <li>Pricing</li>
-        <li>Resources</li>
-      </ul>
+        <ul className="flex items-center gap-4">
+          <li className=" flex items-center gap-1">
+            <p className="text-base font-medium text-[#213343]">Products</p>
+            <IoIosArrowDown />
+          </li>
+          <li className=" flex items-center gap-1">
+            <p className="text-base font-medium text-[#213343]">Solutions</p>
+            <IoIosArrowDown />
+          </li>
+          <li className=" flex items-center gap-1">
+            <p className="text-base font-medium text-[#213343]">Pricing</p>
+            <IoIosArrowDown />
+          </li>
+          <li className=" flex items-center gap-1">
+            <p className="text-base font-medium text-[#213343]">Resources</p>
+            <IoIosArrowDown />
+          </li>
+        </ul>
+      </div>
 
-      <div>
-        <Button className="" value="Get a demo" />
-        <Button className="" value="Get started free" />
+      <div className="flex items-center gap-4">
+        <Button
+          className="border-2 rounded-lg bg-[#ff5c35] border-[#ff5c35] font-bold text-[#fff] py-2 px-4 text-sm"
+          value="Get a demo"
+        />
+        <Button
+          className="border-2 rounded-lg border-[#ff5c35] text-[#ff5c35] font-semibold py-2 px-4 text-sm"
+          value="Get started free"
+        />
       </div>
     </nav>
   );
