8a33999f7de14e86239502e0f66170ee5be0c9e3
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-01-17 06:38:08 +0100

    No changes detected in the current file diff.

---
+++ src/components/YourWholeFront.tsx
@@ -0,0 +1,27 @@
+import Button from "./Button";
+
+export default function YourWholeFront() {
+  return (
+    <section className="bg-[#f6f9fc] w-full lg:px-40 px-4">
+      <main>
+        <img src="" alt="" />
+
+        <div>
+          <h1>Your whole front office. One customer platform.</h1>
+          <p>
+            HubSpot brings your marketing, sales, and service teams together on
+            the same AI-powered customer platform. It's easy to use, provides
+            value fast, and gives all teams a unified view of the customer at
+            every stage in their journey. Each product in the platform is
+            powerful on its own, but the real magic happens when you use them
+            together.
+          </p>
+          <Button className="" value="Get a demo" />
+          <Button className="" value="Get started free" />
+        </div>
+
+        
+      </main>
+    </section>
+  );
+}
