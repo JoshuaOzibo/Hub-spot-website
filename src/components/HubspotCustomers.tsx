0ba44bb9b66772f4affda89d1ff9b5865440f65e
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-02-14 10:51:03 +0100

    updated customer rating section

--- src/components/HubspotCustomers.tsx
+++ src/components/HubspotCustomers.tsx
@@ -1,31 +1,34 @@
 import Button from "./Button";
+import HubspotCustomerLogoOne from '../assets/logo/HubspotLogoOne.svg'
+import HubspotCustomerLogoTwo from '../assets/logo/HubspotLogoTwo.svg'
+import HubspotCustomerLogoThree from '../assets/logo/HubspotLogoThree.svg'
 
 const HubspotCustomers = () => {
   return (
-    <section className="py-12 bg-[#192733] px-5 lg:px-40 ">
-      <h2 className="text-white text-center text-3xl font-bold mb-8">
+    <section className="py-16 bg-[#192733] px-5 lg:px-40 ">
+      <h1 className="text-white text-center text-3xl font-bold mb-8">
         HubSpot customer’s results after 1 year:
-      </h2>
+      </h1>
 
-      <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <div className=" bg-white rounded p-8">
-          <img src="" alt="" />
+      <main className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-6">
+        <div className=" bg-white rounded px-8 py-6">
+          <img className="w-1/2 m-auto" src={HubspotCustomerLogoOne} alt="HubspotCustomerLogoOne" />
           <div className=" space-y-5 text-center">
-            <h1 className="text-2xl font-bold">114%</h1>
+            <h2 className="text-2xl font-bold">114%</h2>
             <p className="text-lg">more web traffic</p>
           </div>
         </div>
-        <div className=" bg-white rounded p-8">
-          <img src="" alt="" />
+        <div className=" bg-white rounded px-8 py-6">
+          <img className="w-1/2 m-auto" src={HubspotCustomerLogoTwo} alt="HubspotCustomerLogoTwo" />
           <div className=" space-y-5 text-center">
-            <h1 className="text-2xl font-bold">55%</h1>
+            <h3 className="text-2xl font-bold">55%</h3>
             <p className="text-lg">more deals</p>
           </div>
         </div>
-        <div className=" bg-white rounded p-8">
-          <img src="" alt="" />
+        <div className=" bg-white rounded px-8 py-6">
+          <img className="w-1/2 m-auto" src={HubspotCustomerLogoThree} alt="HubspotCustomerLogoThree" />
           <div className=" space-y-5 text-center">
-            <h1 className="text-2xl font-bold">43%</h1>
+            <h4 className="text-2xl font-bold">43%</h4>
             <p className="text-lg">more tickets resolved</p>
           </div>
         </div>
