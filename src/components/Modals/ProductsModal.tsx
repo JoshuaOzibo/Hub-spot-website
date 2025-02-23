e784254b85120a810e1506057e6a17993b6d41de
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-02-23 21:17:52 +0100

    updated product page

--- src/components/Modals/ProductsModal.tsx
+++ src/components/Modals/ProductsModal.tsx
@@ -2,9 +2,20 @@
 
 const ProductsModal = () => {
   return (
-    <div className="w-full h-full py-10 bg-red-500">
-      Products
-    </div>
+    <section className="w-full h-full py-10 bg-red-500">
+      <main>
+        <aside className="f">
+          <div>
+          <h1>The Hubspot Customer Platform</h1>
+          <p>All of Hubspot's marketing, sales, and customer service software on one Ai-powered platform.</p>
+          </div>
+          <div>
+            <button>Free Hubspot CRM</button>
+            <button>Overview of all products</button>
+          </div>
+        </aside>
+      </main>
+    </section>
   )
 }
 
