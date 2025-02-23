ad3d60b159dc0b05aea9f9e726019978e51875c1
JoshuaOzibo <joshuamichaelozibo@gmail.com>
2025-02-23 21:21:51 +0100

    updated

--- src/components/Modals/ProductsModal.tsx
+++ src/components/Modals/ProductsModal.tsx
@@ -4,7 +4,7 @@ const ProductsModal = () => {
   return (
     <section className="w-full h-full py-10 bg-red-500">
       <main>
-        <aside className="f">
+        <aside className="">
           <div>
           <h1>The Hubspot Customer Platform</h1>
           <p>All of Hubspot's marketing, sales, and customer service software on one Ai-powered platform.</p>
@@ -14,6 +14,46 @@ const ProductsModal = () => {
             <button>Overview of all products</button>
           </div>
         </aside>
+
+
+        {/* each card items */}
+        <div>
+          <div>
+            <header>Marketing Hub</header>
+            <p>Marketing automation software</p>
+            <a href="##">Free and premium plans</a>
+          </div>
+          <div>
+            <header>Marketing Hub</header>
+            <p>Marketing automation software</p>
+            <a href="##">Free and premium plans</a>
+          </div>
+          <div>
+            <header>Marketing Hub</header>
+            <p>Marketing automation software</p>
+            <a href="##">Free and premium plans</a>
+          </div>
+          <div>
+            <header>Marketing Hub</header>
+            <p>Marketing automation software</p>
+            <a href="##">Free and premium plans</a>
+          </div>
+          <div>
+            <header>Marketing Hub</header>
+            <p>Marketing automation software</p>
+            <a href="##">Free and premium plans</a>
+          </div>
+          <div>
+            <header>Marketing Hub</header>
+            <p>Marketing automation software</p>
+            <a href="##">Free and premium plans</a>
+          </div>
+          <div>
+            <header>Marketing Hub</header>
+            <p>Marketing automation software</p>
+            <a href="##">Free and premium plans</a>
+          </div>
+        </div>
       </main>
     </section>
   )
